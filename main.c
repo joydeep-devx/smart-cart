a940622711164e26555a526f838a1d5f8b542f87
joydeep-devx <sahajoydeep778@gmail.com>
2026-07-10 16:31:21 +0530

    Implement 0/1 Knapsack dynamic programming algorithm

--- main.c
+++ main.c
@@ -25,6 +25,45 @@ void inputProducts(Product products[], int productNo) {
     }
 }
 
+// Returns the maximum utility products using DP
+int knapsack(Product products[], int productNo, int budget) {
+    int dp[productNo + 1][budget + 1];
+
+    // Initialize first column
+    for (int i = 0; i <= productNo; i++) {
+        dp[i][0] = 0;
+    }
+    // Initialize first row 
+    for (int j = 0; j <= budget; j++) {
+        dp[0][j] = 0;
+    }
+
+    // Build DP table
+    for (int i = 1; i <= productNo; i++) {
+        for (int j = 1; j <= budget; j++) {
+
+            // Product's Price is less than current budget
+            if (products[i - 1].price <= j) {
+
+                // utility after including the current product
+                int include = products[i - 1].utility + dp[i - 1][j - products[i - 1].price];
+
+                // utility excluding the current product
+                int exclude = dp[i - 1][j];
+
+                // take maximum between them 
+                dp[i][j] = (include > exclude) ? include : exclude;
+            }
+            // Product's Price is greater than current budget
+            else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    return dp[productNo][budget];
+}
+
 int main() {
     int productNo;
     int budget;
@@ -41,5 +80,8 @@ int main() {
 
     inputProducts(products, productNo);
 
+    int maxUtility = knapsack(products, productNo, budget);
+    printf("\nMaximum Utility: %d\n", maxUtility);
+    
     return 0;
 }
